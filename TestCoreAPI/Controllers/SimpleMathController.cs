b8f63ed6564026076ecb72876c622aba058949d5
Tyler Warren <Tyler.Warren@henryschein.com>
2022-04-20 10:09:39 -0600

    Challenge 1

--- TestCoreAPI/Controllers/SimpleMathController.cs
+++ TestCoreAPI/Controllers/SimpleMathController.cs
@@ -4,13 +4,44 @@ namespace TestCoreAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class SimpleMathController : ControllerBase
+    public class SimpleMathController : Controller
     {
         [HttpGet(Name = "simpleMath")]
         public int simpleMath(int operandOne, int operandTwo, char operation)
         {
-            int result = operandOne + operandTwo;
+            int result;
 
+            //Method 1 - switch statement
+            switch (operation)
+            {
+                case 'S':
+                case 's':
+                    result = operandOne - operandTwo;
+                    break;
+                case 'M':
+                case 'm':
+                    result = operandOne * operandTwo;
+                    break;
+                default:
+                    result = operandOne + operandTwo;
+                    break;
+            }
+
+            /*
+            //Method 2 - if...else statement
+            if ((operation == 'S') || (operation == 's'))
+            {
+                result = operandOne - operandTwo;
+            }
+            else if ((operation == 'M') || (operation == 'm'))
+            {
+                result = operandOne * operandTwo;
+            }
+            else
+            {
+                result = operandOne + operandTwo;
+            }
+            */
             return result;
         }
     }
