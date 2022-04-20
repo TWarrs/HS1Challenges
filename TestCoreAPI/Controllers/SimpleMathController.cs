3bc9b2a1730bf57e30b4ecef9a7f690129142fce
Tyler Warren <Tyler.Warren@henryschein.com>
2022-04-20 10:34:26 -0600

    Challenge 3

--- TestCoreAPI/Controllers/SimpleMathController.cs
+++ TestCoreAPI/Controllers/SimpleMathController.cs
@@ -16,15 +16,23 @@ namespace TestCoreAPI.Controllers
             {
                 case 'S':
                 case 's':
+                case '-':
                     result = operandOne - operandTwo;
                     break;
                 case 'M':
                 case 'm':
+                case '*':
                     result = operandOne * operandTwo;
                     break;
                 case 'D':
                 case 'd':
+                case '/':
                     result = operandOne / operandTwo;
+                    break;
+                case 'R':
+                case 'r':
+                case '%':
+                    result = operandOne % operandTwo;
                     break;
                 default:
                     result = operandOne + operandTwo;
@@ -33,17 +41,21 @@ namespace TestCoreAPI.Controllers
 
             /*
             //Method 2 - if...else statement
-            if ((operation == 'S') || (operation == 's'))
+            if ((operation == 'S') || (operation == 's') || (operation == '-'))
             {
                 result = operandOne - operandTwo;
             }
-            else if ((operation == 'M') || (operation == 'm'))
+            else if ((operation == 'M') || (operation == 'm') || (operation == '*'))
             {
                 result = operandOne * operandTwo;
             }
-            else if ((operation == 'D') || (operation == 'd'))
+            else if ((operation == 'D') || (operation == 'd' || (operation == '/')))
             {
                 result = operandOne / operandTwo;
+            }
+            else if ((operation == 'R') || (operation == 'r' || (operation == '%')))
+            {
+                result = operandOne % operandTwo;
             }
             else
             {
